857bcb713cb1f79f2d14459b478e92c4351e369c
Dmitriy Vilkov <dav.daemon@gmail.com>
2014-03-29 10:07:46 +0400

    Add IDataPlugin interface

---
+++ src/plugins/lvfs_IDataPlugin.h
@@ -0,0 +1,45 @@
+/**
+ * This file is part of lvfs.
+ *
+ * Copyright (C) 2011-2014 Dmitriy Vilkov, <dav.daemon@gmail.com>
+ *
+ * lvfs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lvfs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lvfs. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LVFS_IDATAPLUGIN_H_
+#define LVFS_IDATAPLUGIN_H_
+
+#include <lvfs/Error>
+#include <lvfs/Interface>
+#include <lvfs/plugins/IPlugin>
+
+
+namespace LVFS {
+
+class IDataPlugin : public IPlugin
+{
+    PLATFORM_MAKE_NONCOPYABLE(IDataPlugin)
+    PLATFORM_MAKE_NONMOVEABLE(IDataPlugin)
+    PLATFORM_MAKE_STACK_ONLY
+
+public:
+    IDataPlugin();
+    virtual ~IDataPlugin();
+
+    virtual Interface::Holder open(const Interface::Holder &file, Error &error) const = 0;
+};
+
+}
+
+#endif /* LVFS_IDATAPLUGIN_H_ */
